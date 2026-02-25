cf255e1002811e33904762de3a5c534ff7f423af
Sulaiman Javed <sulaimanjaved2000@gmail.com>
2026-02-26 00:34:02 +0500

    Convert to Maven project structure with org.json dependency

---
+++ src/main/java/com/rest/Main.java
@@ -0,0 +1,30 @@
+package com.rest;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import org.json.JSONObject;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+
+            String apiUrl = "https://jsonmock.hackerrank.com/api/movies";
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(apiUrl))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            JSONObject responseJson = new JSONObject(response.body());
+            System.out.println(response.body());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
